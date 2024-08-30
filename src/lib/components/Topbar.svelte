31394bc4c20733c7aa35e352888fe00d98c9322d
Joel Kallstrom <joelkallstrom@outlook.com>
2024-08-30 21:22:58 +0100

    moves things around

--- src/lib/components/Topbar.svelte
+++ src/lib/components/Topbar.svelte
@@ -1,85 +1,83 @@
 <script>
-  import seriaLogo from '$lib/images/seria-logo-color.png';
-
-  export let navLinks = [
-    { href: '/', label: 'Home' },
-    // TODO: Uncomment the following lines after creating the respective pages
-    // { href: '/about', label: 'About' },
-    // { href: '/services', label: 'Services' },
-    { href: '/contact', label: 'Contact' },
-  ];
+	export let navLinks = [
+		{ href: '/', label: 'Home' },
+		// TODO: Uncomment the following lines after creating the respective pages
+		// { href: '/about', label: 'About' },
+		// { href: '/services', label: 'Services' },
+		{ href: '/contact', label: 'Contact' }
+	];
 </script>
 
 <div class="topbar">
-  <div class="container">
-    <!-- Image and Text container -->
-    <div class="logo-container">
-      <!-- Flex container for logo and text -->
-      <div class="logo">
-        <img src={seriaLogo} alt="Seria Logo" width="60" height="60" />
-      </div>
-      <div class="site-name">
-        Seria <br /> Solutions
-      </div>
-    </div>
-    <nav>
-      <ul class="nav-links">
-        {#each navLinks as link}
-          <li><a href={link.href}>{link.label}</a></li>
-        {/each}
-      </ul>
-    </nav>
-  </div>
+	<div class="container">
+		<!-- Image and Text container -->
+		<div class="logo-container">
+			<!-- Flex container for logo and text -->
+			<div class="logo">
+				<img src="/images/seria-logo-color.png" alt="Seria Logo" width="60" height="60" />
+			</div>
+			<div class="site-name">
+				Seria <br /> Solutions
+			</div>
+		</div>
+		<nav>
+			<ul class="nav-links">
+				{#each navLinks as link}
+					<li><a href={link.href}>{link.label}</a></li>
+				{/each}
+			</ul>
+		</nav>
+	</div>
 </div>
 
 <style>
-  .topbar {
-    background-color: #1e3a8a; /* Blue-600 */
-    color: #FFB400;
-    padding: 8px;
-    min-width: min-content;
-  }
+	.topbar {
+		background-color: #2463eb;
+		color: #ffb400;
+		padding: 8px;
+		min-width: min-content;
+	}
 
-  .container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    max-width: 1200px;
-    margin: 0 auto;
-  }
+	.container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		max-width: 1200px;
+		margin: 0 auto;
+	}
 
-  .logo-container {
-    display: flex;
-    align-items: center;
-  }
+	.logo-container {
+		display: flex;
+		align-items: center;
+	}
 
-  .logo {
-    background-color: white;
-    padding: 8px;
-    flex-shrink: 0;
-  }
+	.logo {
+		background-color: white;
+		padding: 8px;
+		flex-shrink: 0;
+	}
 
-  .site-name {
-    font-size: 1.5rem; /* 2xl */
-    font-weight: bold;
-    margin-left: 8px;
-  }
+	.site-name {
+		font-size: 1.5rem; /* 2xl */
+		font-weight: bold;
+		margin-left: 8px;
+	}
 
-  nav .nav-links {
-    display: flex;
-    gap: 16px; /* Space between nav items */
-    margin-bottom: 24px;
-    margin-left: 24px;
-    list-style: none;
-    padding: 0;
-  }
+	nav .nav-links {
+		display: flex;
+		gap: 16px; /* Space between nav items */
+		margin-bottom: 24px;
+		margin-left: 24px;
+		list-style: none;
+		padding: 0;
+	}
 
-  nav .nav-links a {
-    text-decoration: none;
-    color: inherit;
-  }
+	nav .nav-links a {
+		text-decoration: none;
+		color: inherit;
+	}
 
-  nav .nav-links a:hover {
-    text-decoration: underline;
-  }
+	nav .nav-links a:hover {
+		text-decoration: underline;
+	}
 </style>
